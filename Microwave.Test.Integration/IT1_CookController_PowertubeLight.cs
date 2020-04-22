1deaea1760fe8fb5a21960b7f62a3a253ac0a3b8
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-04-22 17:02:56 +0200

    prøvet at lave et setup til første step cookController powertube og Display

---
+++ Microwave.Test.Integration/IT1_CookController_PowertubeLight.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestClass]
+    public class IT1_CookController_PowertubeLight
+    {
+        
+        private IOutput _output;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ITimer _timer;
+        private IUserInterface _userInterface; 
+        private CookController _uut; 
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+            _userInterface = Substitute.For<IUserInterface>();  // nødtil eller kan jeg ikke oprette en instans af cookController når vi deler dependencies op i flere steps? 
+            _timer = Substitute.For<ITimer>();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+
+            _uut = new CookController(_timer,_display, _powerTube, _userInterface);
+        }
+
+    }
+}
---
+++ Microwave.Test.Integration/IT1_Powertube_Output.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOven.MicrowaveOvenClasses.interfaces;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT1_Powertube_Output
+    {
+        private IOutput _ouput; 
+
+    }
+}
