a4e05f3d951b94b82bae18114db4b59edebf0dd0
Quynh Trang Mac <201810269@post.au.dk>
2020-04-26 14:53:38 +0200

    Added showTime with event to IT1

--- Microwave.Test.Integration/IT1_CookController_PowertubeDisplay.cs
+++ Microwave.Test.Integration/IT1_CookController_PowertubeDisplay.cs
@@ -1,38 +1,46 @@
 ﻿using System;
 using System.IO;
+using System.Timers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using NSubstitute.ReceivedExtensions;
 using NUnit.Framework;
 //using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 using Assert = NUnit.Framework.Assert;
+using Timer = MicrowaveOvenClasses.Boundary.Timer;
 
 namespace Microwave.Test.Integration
 {
     [TestClass]
     public class IT1_CookController_PowertubeDisplay
     {
-        
+
         private IOutput _output;
         private IDisplay _display;
         private IPowerTube _powerTube;
         private ITimer _timer;
-        private IUserInterface _userInterface; 
-        private CookController _uut; 
+        private IUserInterface _userInterface;
+        private CookController _uut;
+
 
 
         [SetUp]
         public void SetUp()
         {
             _output = Substitute.For<IOutput>();
-            _userInterface = Substitute.For<IUserInterface>();  // nødtil eller kan jeg ikke oprette en instans af cookController når vi deler dependencies op i flere steps? 
+            _userInterface =
+                Substitute
+                    .For<IUserInterface
+                    >(); // nødtil eller kan jeg ikke oprette en instans af cookController når vi deler dependencies op i flere steps? 
             _timer = Substitute.For<ITimer>();
             _powerTube = new PowerTube(_output);
             _display = new Display(_output);
 
-            _uut = new CookController(_timer,_display, _powerTube, _userInterface);
+            _uut = new CookController(_timer, _display, _powerTube, _userInterface);
         }
 
         [TestCase(1, 60)]
@@ -40,7 +48,7 @@ namespace Microwave.Test.Integration
         [TestCase(99, 60)]
         [TestCase(100, 60)]
         //[TestCase(700, 60)]       //kig på opgaven - der står 700
-        public void CookCtrTurnOnPowerTube_TimeSixty(int power,int time)
+        public void CookCtrTurnOnPowerTube_TimeSixty(int power, int time)
         {
             _uut.StartCooking(power, time);
             _output.Received().OutputLine($"PowerTube works with {power}");
@@ -64,9 +72,23 @@ namespace Microwave.Test.Integration
         public void StartCooking_Twice(int power, int time)
         {
             _uut.StartCooking(power, time);
-            
-            Assert.That(()=> _uut.StartCooking(power, time), Throws.TypeOf<ApplicationException>());
+
+            Assert.That(() => _uut.StartCooking(power, time), Throws.TypeOf<ApplicationException>());
         }
+
+
+        /*-------------------------Display-------------------------------*/
+
+        [TestCase(10,10)]
+
+        public void ShowTimeCorrectly(int min, int sec)
+        {
+
+            _uut.StartCooking(min,sec);
+            _timer.TimerTick += Raise.EventWith<EventArgs>(EventArgs.Empty);
+            _output.OutputLine($"Display shows: {min:D2}:{sec:D2}");
+        }
+
 
     }
 }
