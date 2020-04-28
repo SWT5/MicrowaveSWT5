6460c0ad79570b61e4e48a1c9a8afa04361338a1
Thanh Tieu Pham <au479218@uni.au.dk>
2020-04-28 08:18:13 +0200

    setUp IT3

---
+++ Microwave.Test.Integration/IT3_UI_DisplayLight.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestClass]
+    public class IT3_UI_DisplayLight
+    {
+        private IOutput _output;
+        private IPowerTube _powerTube;
+        private IDisplay _display;
+        private ILight _light;
+        private ICookController _cookController;
+        private ITimer _timer;
+        
+        private IUserInterface _userInterface;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+
+        private IDoor _door;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _startCancelButton = Substitute.For<IButton>();
+            _door = Substitute.For<IDoor>();
+            _output = Substitute.For<IOutput>();
+
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _cookController = new CookController(_timer, _display, _powerTube, _userInterface);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+        }
+
+        /*----------   UI/Light     ----------------*/
+
+        [Test]
+        public void UI_DoorOpenedEvent_TurnOnLight()
+        {
+
+        }
+
+
+
+
+        /*----------   UI/Display     ----------------*/
+
+    }
+}
+
