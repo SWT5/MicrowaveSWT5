1d23a4079f8632d1462238eea977caba6b2c301f
Mads Bo Mikkelsen <au615041@uni.au.dk>
2020-04-28 13:58:36 +0200

    rettet unittest for timer da time er rettet fra 1000 til 1 skal der rettes fra uut.start(2000) til uut.start(2)

--- Microwave.Test.Unit/TimerTest.cs
+++ Microwave.Test.Unit/TimerTest.cs
@@ -21,7 +21,7 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.TimerTick += (sender, args) => pause.Set();
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait for a tick, but no longer
             Assert.That(pause.WaitOne(1100));
@@ -45,7 +45,7 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.Expired += (sender, args) => pause.Set();
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait for expiration, but not much longer, should come
             Assert.That(pause.WaitOne(2100));
@@ -72,7 +72,7 @@ namespace Microwave.Test.Unit
             uut.Expired += (sender, args) => pause.Set();
             uut.TimerTick += (sender, args) => notifications++;
 
-            uut.Start(2000);
+            uut.Start(2);
 
             // wait longer than expiration
             Assert.That(pause.WaitOne(2100));
